0dd7bd4f7e2803db816f2b6dbdff5072ab2f64ed
LukasG85 <lukas85b@gmail.com>
2019-09-29 15:26:18 +0200

    Updates

---
+++ precache-manifest.936553917eb91b3468cdee1ec5cb085d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b41e514035976f71bace2e6eceac7072",
+    "url": "/hex/index.html"
+  },
+  {
+    "revision": "d2906dde2da6aeb9ef68",
+    "url": "/hex/static/css/main.c13ce44b.chunk.css"
+  },
+  {
+    "revision": "69d1a48368197fda919f",
+    "url": "/hex/static/js/2.b19aea10.chunk.js"
+  },
+  {
+    "revision": "d2906dde2da6aeb9ef68",
+    "url": "/hex/static/js/main.1540da3b.chunk.js"
+  },
+  {
+    "revision": "e30dc8b0af5391916ec8",
+    "url": "/hex/static/js/runtime-main.15743894.js"
+  },
+  {
+    "revision": "3f65978fbf3f22b12eecc7a9544de5db",
+    "url": "/hex/static/media/restaurant4.3f65978f.jpg"
+  }
+]);
